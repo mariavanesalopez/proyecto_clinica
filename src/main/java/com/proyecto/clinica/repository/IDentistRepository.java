55b6f4ce2ee605937e86694412c531faffd8dcea
mariavanesalopez <mvanelopezsueldo579@gmail.com>
2022-12-11 22:23:02 -0300

    hql

--- src/main/java/com/proyecto/clinica/repository/IDentistRepository.java
+++ src/main/java/com/proyecto/clinica/repository/IDentistRepository.java
@@ -2,8 +2,14 @@ package com.proyecto.clinica.repository;
 
 import com.proyecto.clinica.entity.Dentist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IDentistRepository extends JpaRepository<Dentist, Long> {
+
+    @Query("FROM Dentists d WHERE d.name = ?1 AND d.surname = ?2")
+    Dentist searchDentist(String name, String surname);
+
+
 }
--- src/main/java/com/proyecto/clinica/repository/IPatientRepository.java
+++ src/main/java/com/proyecto/clinica/repository/IPatientRepository.java
@@ -1,9 +1,17 @@
 package com.proyecto.clinica.repository;
 
+import com.proyecto.clinica.entity.Dentist;
 import com.proyecto.clinica.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+
 
 @Repository
 public interface IPatientRepository extends JpaRepository<Patient, Long> {
+
+    @Query("FROM Patients p WHERE p.name = ?1 AND p.surname = ?2")
+    Patient searchPatient(String name, String surname);
+
 }
